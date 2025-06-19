ae2bc614df68e432d0db689594addfb100642214
Kaspador <kaspador@gmail.com>
2025-06-19 10:41:07 +0300

    Update head.js with social meta tags

--- app/head.js
+++ app/head.js
@@ -4,6 +4,18 @@ export default function Head() {
       <title>Kaspaiq.com</title>
       <meta name="description" content="Send $kas to find out your IQ!" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://kaspaiq.vercel.app/" />
+      <meta property="og:title" content="Kaspaiq.com" />
+      <meta property="og:description" content="Send $kas to find out your IQ!" />
+      <meta property="og:image" content="/kaspaiq-preview.png" />
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:url" content="https://kaspaiq.vercel.app/" />
+      <meta name="twitter:title" content="Kaspaiq.com" />
+      <meta name="twitter:description" content="Send $kas to find out your IQ!" />
+      <meta name="twitter:image" content="/kaspaiq-preview.png" />
     </>
   );
 } 
